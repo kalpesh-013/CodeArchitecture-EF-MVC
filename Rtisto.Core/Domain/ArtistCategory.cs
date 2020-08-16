4340a5bbc7504e280a9ef317eb6e3d26a694248e
kalpesh-013 <69079870+kalpesh-013@users.noreply.github.com>
2020-08-16 19:14:36 +0530

    Add files via upload

---
+++ Rtisto.Core/Domain/ArtistCategory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rtisto.Core.Domain
+{
+    [Table("ArtistCategory")]
+    public class ArtistCategory : DelEntity
+    {
+        [Key]
+        [Column("Id")]
+        public int Id { get; set; }
+
+        [Column("Title")]
+        public string Title { get; set; }
+
+        [Column("IsActive")]
+        public bool IsActive { get; set; }
+    }
+}
---
+++ Rtisto.Core/Domain/DelEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rtisto.Core.Domain
+{
+    public class DelEntity
+    {
+        [Column("IsDeleted")]
+        public bool IsDeleted { get; set; }
+    }
+}
